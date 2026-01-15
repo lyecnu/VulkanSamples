1b11a5204a20af5deab9edef496e0cd48443b4e3
kabutack <lyecnu@qq.com>
2026-01-15 19:57:29 +0800

    完善build.bat

--- shaders/samples/build.bat
+++ shaders/samples/build.bat
@@ -19,32 +19,31 @@ set "FAILED_FILE=%TEMP%\shader_build_failed.txt"
 if exist "%FAILED_FILE%" del "%FAILED_FILE%" >nul 2>&1
 
 echo Compiling GLSL shaders to SPIR-V (target: Vulkan 1.3)...
+echo.
 
 for %%e in (vert frag comp) do (
 	for /R "%CD%" %%f in (*.%%e) do (
 		if /I NOT "%%~dpf"=="%CURDIR%" (
 			set /a TOTAL+=1
-			echo.
 			set "full=%%f"
 			set "rel=!full:%CURDIR%=!"
 			if "!rel!"=="!full!" set "rel=%%~nxf"
-			echo Compiling "!rel!"...
 			set "out=%%~dpf%%~nxf.spv"
-			set "outrel=!out:%CURDIR%=!"
-			echo   Output: "!outrel!"
-			glslangValidator -V --target-env vulkan1.3 -o "!out!" "%%f"
+			glslangValidator -V --target-env vulkan1.3 -o "!out!" "%%f" 2>&1 > temp_log.txt
+			findstr /C:"ERROR" temp_log.txt >nul
 			if errorlevel 1 (
-				echo FAILED: "!rel!"
-				set /a FAIL+=1
-				echo !rel!>>"%FAILED_FILE%"
-			) else (
 				set /a SUCCESS+=1
+			) else (
+				type temp_log.txt
+				echo(
+				set /a FAIL+=1
 			)
+			del temp_log.txt
 		)
 	)
 )
 
-echo.
+echo(
 echo Compilation summary:
 echo   Total: !TOTAL!
 echo   Success: !SUCCESS!
@@ -56,5 +55,5 @@ if exist "%FAILED_FILE%" (
 )
 
 echo.
-pause
+set /p dummy=Press Enter To Continue..
 endlocal
