cd69649127268962e6bfe18a8b2b86fb1d063ec4
Kabutack <lyecnu@qq.com>
2026-02-09 17:31:25 +0800

    添加框架

---
+++ samples/stencilbuffer/stencilbuffer.cpp
@@ -0,0 +1,104 @@
+﻿#include "vulkanexamplebase.h"
+#include "VulkanglTFModel.h"
+
+class VulkanExample : public VulkanExampleBase
+{
+public:
+	vkglTF::Model model;
+
+	struct
+	{
+		glm::mat4 projection;
+		glm::mat4 view;
+	} uniformData;
+	std::array<vks::Buffer, maxConcurrentFrames> uniformBuffers;
+
+	VulkanExample(): VulkanExampleBase()
+	{
+		title = "Stencil Buffer";
+
+		apiVersion = VK_API_VERSION_1_3;
+
+		enabledFeatures.dynamic
+	}
+
+	void loadAssets()
+	{
+		uint32_t flags = vkglTF::FileLoadingFlags::PreTransformVertices | vkglTF::FileLoadingFlags::FlipY | vkglTF::FileLoadingFlags::PreMultiplyVertexColors;
+		model.loadFromFile(getAssetPath()+ "models/venus.gltf", vulkanDevice, queue, flags);
+	}
+
+	void prepareUniformBuffers()
+	{
+		for (auto& buffer : uniformBuffers)
+		{
+			 VK_CHECK_RESULT(vulkanDevice->createBuffer(VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, &buffer, sizeof(uniformData), nullptr));
+			 VK_CHECK_RESULT(buffer.map());
+		}
+	}
+
+	void prepare() override
+	{
+		VulkanExampleBase::prepare();
+		loadAssets();
+		prepareUniformBuffers();
+
+		prepared = true;
+	}
+
+	void updateUniformBuffers()
+	{
+		uniformData.projection = camera.matrices.perspective;
+		uniformData.view = camera.matrices.view;
+	}
+
+	void buildCommandBuffer()
+	{
+		VkCommandBuffer cmdBuffer = drawCmdBuffers[currentBuffer];
+
+		VkCommandBufferBeginInfo cmdBufInfo = vks::initializers::commandBufferBeginInfo();
+		VK_CHECK_RESULT(vkBeginCommandBuffer(cmdBuffer, &cmdBufInfo));
+
+		VkRenderingAttachmentInfo colorAttachment{ .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO };
+		colorAttachment.imageView = swapChain.imageViews[currentImageIndex];
+		colorAttachment.imageLayout = VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL;
+		colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		colorAttachment.clearValue = { 0.1f, 0.1f, 0.1f, 1.0f };
+
+		VkRenderingAttachmentInfo depthStencilAttachment{ .sType = VK_STRUCTURE_TYPE_RENDERING_ATTACHMENT_INFO };
+		depthStencilAttachment.imageView = depthStencil.view;
+		depthStencilAttachment.imageLayout = VK_IMAGE_LAYOUT_ATTACHMENT_OPTIMAL;
+		depthStencilAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		depthStencilAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+		depthStencilAttachment.clearValue = { 1.0f, 0 };
+
+		VkRenderingInfo renderingInfo{ .sType = VK_STRUCTURE_TYPE_RENDERING_INFO };
+		renderingInfo.renderArea = { {0, 0}, width, height };
+		renderingInfo.layerCount = 1;
+		renderingInfo.colorAttachmentCount = 1;
+		renderingInfo.pColorAttachments = &colorAttachment;
+		renderingInfo.pDepthAttachment = &depthStencilAttachment;
+		renderingInfo.pStencilAttachment = &depthStencilAttachment;
+
+		vkCmdBeginRendering(cmdBuffer, &renderingInfo);
+
+		model.draw(cmdBuffer);
+
+		vkCmdEndRendering(cmdBuffer);
+
+		vkEndCommandBuffer(cmdBuffer);
+		
+	}
+
+	void render() override
+	{
+		if (!prepared) return;
+		VulkanExampleBase::prepareFrame();
+		updateUniformBuffers();
+		buildCommandBuffer();
+		VulkanExampleBase::submitFrame();
+	}
+};
+
+VULKAN_EXAMPLE_MAIN()
